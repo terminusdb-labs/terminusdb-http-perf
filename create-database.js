7743fab05b2b4724ff36cf29214e11818aa46a5f
Sean Leather <sean.leather@gmail.com>
2021-07-20 15:19:22 +0200

    Add create-database.js, create-delete-database.js

---
+++ create-database.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export default function () {
+  const databaseName = `database-${__VU}-${__ITER}`;
+  const body = { comment: 'New database', label: databaseName };
+  const params = { headers: { 'Content-Type': 'application/json' } };
+  let response =
+    http.post(
+      `http://admin:root@127.0.0.1:6363/api/db/admin/${databaseName}`,
+      JSON.stringify(body), params);
+  sleep(1);
+
+  // Verify response
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+  });
+}
---
+++ create-delete-database.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export default function () {
+  const databaseName = `database-${__VU}-${__ITER}`;
+  const body = { comment: 'New database', label: databaseName };
+  const params = { headers: { 'Content-Type': 'application/json' } };
+
+  let response1 =
+    http.post(
+      `http://admin:root@127.0.0.1:6363/api/db/admin/${databaseName}`,
+      JSON.stringify(body), params);
+
+  check(response1, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+
+  let response2 =
+    http.del(`http://admin:root@127.0.0.1:6363/api/db/admin/${databaseName}`);
+
+  check(response2, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
