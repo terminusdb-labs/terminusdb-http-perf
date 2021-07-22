b5c1957087201fb4d25b3185ad756628700310fe
Sean Leather <sean.leather@gmail.com>
2021-07-22 15:05:54 +0200

    Make create-database.js like delete-database.js

--- create-database.js
+++ create-database.js
@@ -1,29 +1,51 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+// Database create response time.
+//
+// Look at { scenario:default } under `http_req_duration`.
 
+import http from 'k6/http';
+import { fail, sleep } from 'k6';
+
+export let options = {
+  // See <https://community.k6.io/t/ignore-http-calls-made-in-setup-or-teardown-in-results/878/2>
+  // This allows us to look at only the HTTP requests in `default` and ignore
+  // the longer times in `setup`, which involve creating the database.
+  thresholds: {
+    'http_req_duration{scenario:default}': ['max>=0'],
+  },
+};
+
+function databaseNameOfIter(data, iter) {
+  return `${data.databaseNamePrefix}-${iter}`;
+}
+
+// setup:
+// 1. Define a unique database name prefix to avoid name clashes.
 export function setup() {
   return {
-    // A random string hopefully unique to this run.
-    rand: Math.random().toString(36).substring(2)
+    databaseNamePrefix: `database-${Math.random().toString(36).substring(2)}`,
   };
 }
 
+const params = { headers: { 'Content-Type': 'application/json' } };
+
+// default:
+// 1. Create the database for this iteration.
 export default function (data) {
-  // Use a random string in the database name to avoid clashing with
-  // pre-existing database names.
-  const databaseName = `database-${data.rand}-${__VU + __ITER}`;
-
-  const body = { comment: 'New database', label: databaseName };
-  const params = { headers: { 'Content-Type': 'application/json' } };
-
-  const response =
-    http.post(
-      `http://admin:root@127.0.0.1:6363/api/db/admin/${databaseName}`,
-      JSON.stringify(body), params);
-
-  check(response, {
-    'status is 200': (r) => r.status === 200,
-  });
-
+  const databaseName = databaseNameOfIter(data, __ITER);
+  const url = `http://admin:root@127.0.0.1:6363/api/db/admin/${databaseName}`;
+  const body = JSON.stringify({ comment: 'comment', label: databaseName });
+  http.post(url, body, params).status === 200 ||
+    fail(`could not create: ${databaseName}`);
   sleep(1);
+}
+
+// teardown:
+// 1. Delete the databases for all iterations.
+export function teardown(data) {
+  for (let iter = 0; iter < (options.iterations || 1); iter++) {
+    const databaseName = databaseNameOfIter(data, iter);
+    const url = `http://admin:root@127.0.0.1:6363/api/db/admin/${databaseName}`;
+    http.del(url).status === 200 ||
+      fail(`could not delete: ${databaseName}`);
+  }
 }
