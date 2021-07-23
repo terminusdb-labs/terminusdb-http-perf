5e7aa4408758549ab7121282f140a4c3cc54723d
Sean Leather <sean.leather@gmail.com>
2021-07-23 12:51:44 +0200

    Bring info.js, ping.js into line

--- info.js
+++ info.js
@@ -1,7 +1,13 @@
+// Info
+
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+// default:
+// 1. Send GET to `/api/info`.
 export default function () {
-  http.get('http://127.0.0.1:6363/api/info');
+  const status = http.get('http://127.0.0.1:6363/api/info').status;
+  status === 200 ||
+    fail(`unexpected status: ${status}`);
   sleep(1);
 }
--- ping.js
+++ ping.js
@@ -1,11 +1,13 @@
+// Ok
+
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { fail, sleep } from 'k6';
 
+// default:
+// 1. Send GET to `/api/ok`.
 export default function () {
-  let response = http.get('http://127.0.0.1:6363/api/ok');
+  const status = http.get('http://127.0.0.1:6363/api/ok').status;
+  status === 200 ||
+    fail(`unexpected status: ${status}`);
   sleep(1);
-
-  check(response, {
-    'status is 200': (r) => r.status === 200,
-  });
 }
